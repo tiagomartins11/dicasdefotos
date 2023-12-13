044743dd2d55450c061a29cbb384ae191b0c42cc
tiagomartins11 <tiago.paiva.martins11@gmail.com>
2023-12-13 15:25:31 -0300

    0.1.0 Criação da tela inicial Possui a Logo e um dois breves textos do que tem no aplicativo. Também tem um botão para avançar para a segunda tela

--- app/src/main/java/com/example/dicasdefotos/MainActivity.kt
+++ app/src/main/java/com/example/dicasdefotos/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.dicasdefotos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //variável  para fazer o botão ir para proxima tela
+        val butao = findViewById<Button>(R.id.button_avancar1)
+
+
+        butao.setOnClickListener{ charSegundoTela()
+        }
+    }
+    //função indincando para onde vai ao clicar no botão
+    private fun charSegundoTela() {
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
     }
 }
